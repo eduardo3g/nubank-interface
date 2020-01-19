a2da15fa4df47e415343557f60288cc9f38a31aa
= <=>
2020-01-19 16:37:19 -0300

    chore: Main content styles.

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   height: 100px;
   margin-top: 20px;
+  margin-bottom: 20px;
 `;
 
 export const TabsContainer = styled.ScrollView.attrs({
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <MaterialIcons name="attach-money" size={28} color="#666" />
+            <MaterialIcons name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 980.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 783,00 recebida de Paula Lima hoje às 06:32h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
