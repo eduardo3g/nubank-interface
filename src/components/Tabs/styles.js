acd5bf81f14482d011ca99821cc8d3ddaaa6bdc2
= <=>
2020-01-19 16:25:32 -0300

    fix: Added margin top on Tabs compoent.

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  height: 100px;
+  margin-top: 20px;
+`;
 
 export const TabsContainer = styled.ScrollView.attrs({
   horizontal: true,
