21e1e1eff84cf9fb932c35af5cd5f343bdf6a252
= <=>
2020-01-19 16:04:55 -0300

    chore: Tabs component styled.

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -1,3 +1,24 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const TabsContainer = styled.ScrollView.attrs({
+  horizontal: true,
+  contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const TabItem = styled.View`
+  width: 100px;
+  height: 100px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 3px;
+  margin-left: 10px;
+  padding: 10px;
+  justify-content: space-between;
+`;
+
+export const TabText = styled.Text`
+  font-size: 13px;
+  color: #fff;
+`;
